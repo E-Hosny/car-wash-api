6a145ba3dacee8932c7f62ecebb13e54ddf24d1d
test <ebrahimhosny511@gmail.com>
2025-06-28 10:29:24 +0300

    Remove Firebase service account credentials and move to environment variables

--- app/Services/FirebaseNotificationService.php
+++ app/Services/FirebaseNotificationService.php
@@ -6,7 +6,12 @@ use Illuminate\Support\Facades\Http;
 
 class FirebaseNotificationService
 {
-    protected $projectId = 'carwash-24656'; // 👈 غيّرها
+    protected $projectId;
+
+    public function __construct()
+    {
+        $this->projectId = env('FCM_PROJECT_ID', 'carwash-24656');
+    }
 
     public function sendToToken($token, $title, $body)
     {
@@ -32,8 +37,24 @@ class FirebaseNotificationService
     private function getAccessToken()
     {
         $client = new \Google_Client();
-        $client->setAuthConfig(storage_path('firebase/firebase-service-account.json'));
+        
+        // Use service account credentials from environment variables
+        $credentials = [
+            'type' => 'service_account',
+            'project_id' => env('FCM_PROJECT_ID'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+            'private_key' => str_replace('\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => env('FIREBASE_CLIENT_ID'),
+            'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri' => 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL'),
+        ];
+        
+        $client->setAuthConfig($credentials);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        
         return $client->fetchAccessTokenWithAssertion()['access_token'];
     }
 }
